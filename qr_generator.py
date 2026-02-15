60d59356bc1bb44de819368f201f2fc4b208d1aa
Charlie <109276389+Fruitc4ke@users.noreply.github.com>
2026-02-15 01:35:09 +0100

    Update qr_generator.py

--- qr_generator.py
+++ qr_generator.py
@@ -1,15 +1,13 @@
+import sys
 import qrcode
 
-# Ask user for hyperlink
-url = input("Quel lien voulez-vous convertir en QR code ?: ")
+if len(sys.argv) > 1:
+    url = sys.argv[1]
+else:
+    print("Please provide a URL")
+    sys.exit()
 
-if not url.startswith("http"):
-    print("! Attention : une URL doit commencer par http ou https !")
-
-# Generate QR code
 qr = qrcode.make(url)
-
-# Save image
 qr.save("generated_qr.png")
 
 print("QR code saved as generated_qr.png")
